e5d4f358fd5db3a91b1ad543282de68216dedd4e
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-05-28 11:52:55 -0500

    additional css added

---
+++ app.js
@@ -0,0 +1,27 @@
+const nextPoster = () => {
+  // change background
+  // let poster = document.querySelector(".posterContainer").style.background-color="purple"
+
+  // replace image
+  let newImage = document.createElement("img");
+  newImage.setAttribute(
+    "src",
+    "https://static.wikia.nocookie.net/disney/images/6/68/Little-mermaid-disneyscreencaps.com-4723.jpg/revision/latest/scale-to-width-down/250?cb=20191229011459"
+  );
+  newImage.setAttribute("class", "image");
+  let originalImage = document.querySelector(".image");
+  originalImage.replaceWith(newImage);
+
+  // replace name text
+  let name = document.querySelector(".nameText");
+  name.innerHTML = "Ursela the Sea Witch";
+
+  //replace description text
+  let description = document.querySelector(".descriptionText");
+  description.innerHTML =
+    "Description: Purple skin, white hair, known for making deals with young mermaids to steal their voices";
+
+  //replace reward
+  let reward = document.querySelector(".reward");
+  reward.innerHTML = "2,000,000 shells";
+};
